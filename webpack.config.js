d48eef9058ff4cb34f706fdd53dadf22f84c1632
luizmarcello <luizmarcello@DESKTOP-C7614T3>
2023-12-03 10:56:37 -0300

    03.12.23_ProjetoOriginalOk

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,39 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 /* Aqui é onde "tudo" vai acontecer */
 module.exports = {
     /* Ponto de entrada */
-    entry: './src/main.js'
+    /* Neste arquivo, tudo vai ser compilado */
+    entry: './src/main.js',
+    /* Vai ser compilado para este lugar */
+    output: {
+        path: process.env.NODE_ENV == 'development' ? __dirname : path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "main.css"
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.json', '.vue'],
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, "css-loader"
+                ]
+            }
+
+        ]
+    }
+
 }
