60866bec2d9ad8870d4cf7ea1b16640eae02e280
root <root@DESKTOP-C7614T3>
2023-12-09 15:19:22 -0300

    09.12.23_15:19hs

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+Use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+/* api/users */
+Route::get('/users', function(){
+    return User::all();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
